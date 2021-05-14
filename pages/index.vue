d479821cf96ded658074e59b758a00fef8c13eba
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-05-14 09:40:03 +0200

    done

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="toast">toast me!</button>
+  </div>
+</template>
+
+<script>
+import Toast from 'toastr'
+
+export default {
+  methods: {
+    toast() {
+      Toast.warning('My name is Inigo Montoya. You killed my father, prepare to die!')
+    },
+  },
+}
+</script>
+
+<style>
+@import "toastr";
+</style>
